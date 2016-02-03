1140d59ff73b274dec2e8e7ef303424a0fb463b3
alefsilva <alefsilva@prefeitura.sp.gov.br>
2016-02-03 10:49:06 -0200

    Subir Arquivo de Imagem do produto

--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.admin')
 	@section('content')
-			  	{!!Form::open(['route'=>'product.store', 'method'=>'POST'])!!}
+			  	{!!Form::open(['route'=>'product.store', 'method'=>'POST','files'=> true])!!}
 		  		@include('product.forms.product')
 				{!!Form::submit('Registrar',['class'=>'btn btn-primary'])!!}
 			{!!Form::close()!!}
